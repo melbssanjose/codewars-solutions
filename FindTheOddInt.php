39bc13a62be482a3ff49aa4b20c61298f9b877c3
nuskumelbs <37319374+nuskumelbs@users.noreply.github.com>
2018-03-13 02:22:17 -0700

    Create FindTheOddInt.php

---
+++ FindTheOddInt.php
@@ -0,0 +1,15 @@
+/**
+  * Given an array, find the int that appears an odd number of times.
+  *
+  * There will always be only one integer that appears an odd number of times.
+ **/
+function findIt(array $seq) : int
+{
+  $x = 0;
+  
+  foreach($seq as $data){
+    $x ^= $data;
+  }
+  
+  return $x;
+}
