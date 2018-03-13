d3b5ed214bbdb8cf16b501e97dc3d34b2a005c40
nuskumelbs <37319374+nuskumelbs@users.noreply.github.com>
2018-03-12 21:14:46 -0700

    Create Tribonacci.php

---
+++ Tribonacci.php
@@ -0,0 +1,11 @@
+
+function tribonacci($signature, $n) {
+  
+  if($n<=3) return array_slice($signature, 0, $n);
+  
+  for($i = 3; $i < $n; $i++){
+    $signature[$i] = $signature[$i-1] + $signature[$i-2] + $signature[$i-3];
+  }
+  
+  return $signature;
+}
