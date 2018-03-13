7874643dbc6d3c446833733cadb58957c710982b
nuskumelbs <37319374+nuskumelbs@users.noreply.github.com>
2018-03-12 22:35:04 -0700

    Update ConsecutiveStrings.php

--- ConsecutiveStrings.php
+++ ConsecutiveStrings.php
@@ -1,3 +1,11 @@
+/** You are given an array strarr of strings and an integer k. Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+
+  * #Example: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+
+  * n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+                                                                  
+  **/
+                                                                      
 function longestConsec($strarr, $k) {
   $longest = "";
   $n = count($strarr);
