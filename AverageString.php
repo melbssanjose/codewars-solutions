b863b74ba9b54006e8c8b23d17bf07705de5161c
nuskumelbs <37319374+nuskumelbs@users.noreply.github.com>
2018-03-14 22:10:28 -0700

    Update AverageString.php

--- AverageString.php
+++ AverageString.php
@@ -1,3 +1,4 @@
+<?php
 /**
  *
  * You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
