769674edd9f529573a16842bde0f41a06e2f6df6
nuskumelbs <37319374+nuskumelbs@users.noreply.github.com>
2018-03-14 22:10:04 -0700

    Update ListSquared.php

--- ListSquared.php
+++ ListSquared.php
@@ -4,42 +4,32 @@
 
  * The result will be an array of arrays or of tuples (in C an array of Pair) or a string, each subarray having two elements, first the number whose squared divisors is a square and then the sum of the squared divisors.
  **/
-function listSquared($m, $n) {
-  $perfectSum = array();
-  
-  for($i=$m;$i<=$n;$i++){
-    $divisors = getDivisors($i);
 
-    $count = count($divisors);
-    $sumSquared = getSumSquared($divisors);
-    
-    if(isPerfectSquare($sumSquared)){
-      $perfectSum[] = [$i, $sumSquared];
+<?php
+  function listSquared($m, $n) {
+    $perfectSum = array();
+
+    for($i=$m;$i<=$n;$i++){
+      $squareDivisors = getSquareDivisors($i);
+
+      if(isPerfectSquare($squareDivisors)){
+        $perfectSum[] = [$i, $squareDivisors];
+      }
     }
+    return $perfectSum;
   }
-  return $perfectSum;
-}
 
-function getDivisors($n){
-  $divisors = array();
-  
-  for($i=1;$i<=$n;$i++){
-    if($n%$i==0) $divisors[] = $i;
-  }
-  
-  return $divisors;
-}
-
-function isPerfectSquare($n){
-  $root = sqrt($n);
- 
-  return $root == (int)$root;
-}
-
-function getSumSquared(array $n){
+  function getSquareDivisors($n){
     $sum = 0;
-    for($i=0;$i<count($n);$i++){
-        $sum += $n[$i] * $n[$i];
+    for($i=1;$i<=$n;$i++){
+      if($n%$i==0) $sum += $i*$i;
     }
+
     return $sum;
-}
+  }
+
+  function isPerfectSquare($n){
+    $root = sqrt($n);
+
+    return $root == (int)$root;
+  }
